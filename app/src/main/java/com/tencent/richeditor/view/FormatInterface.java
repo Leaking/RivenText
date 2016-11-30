5c1e3f29c7e58db03ee674346f64c8731337f4b0
Quinn <chenhuazhaoao@gmail.com>
2016-11-30 20:11:37 +0800

    commit

--- app/src/main/java/com/tencent/richeditor/view/FormatInterface.java
+++ app/src/main/java/com/tencent/richeditor/view/FormatInterface.java
@@ -6,6 +6,24 @@ package com.tencent.richeditor.view;
 
 public interface FormatInterface {
 
+    public void bold(int start, int end, boolean format);
+
+    public boolean cantainBold(int start, int end);
+
+    public void italic(int start, int end, boolean format);
+
+    public boolean containItalic(int start, int end);
+
+    public void underLine(int start, int end, boolean format);
+
+    public boolean containUnderLine(int start, int end);
+
+    public void strikeThrough(int start, int end, boolean format);
+
+    public boolean containStrikeThrough(int start, int end);
+
+    public void addCheckBox();
+
 
 
 
--- app/src/main/java/com/tencent/richeditor/view/RivenText.java
+++ app/src/main/java/com/tencent/richeditor/view/RivenText.java
@@ -1,13 +1,17 @@
 package com.tencent.richeditor.view;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.util.AttributeSet;
 
 /**
  * Created by quinn on 11/30/16.
  */
 
-public class RivenText extends android.support.v7.widget.AppCompatEditText {
+public class RivenText extends android.support.v7.widget.AppCompatEditText implements FormatInterface {
 
 
     public RivenText(Context context) {
@@ -23,9 +27,75 @@ public class RivenText extends android.support.v7.widget.AppCompatEditText {
     }
 
 
+    @Override
+    public void bold(int start, int end, boolean format) {
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        if(format) {
+            getEditableText().setSpan(boldSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        } else {
+            StyleSpan[] spans = getEditableText().getSpans(start, end, StyleSpan.class);
+            for (StyleSpan span : spans) {
+                if (span.getStyle() == Typeface.BOLD) {
+                    getEditableText().removeSpan(span);
+                }
+            }
+        }
+    }
 
+    @Override
+    public boolean cantainBold(int start, int end) {
+        StyleSpan[] spans = getEditableText().getSpans(start, end, StyleSpan.class);
+        for (StyleSpan span : spans) {
+            if (span.getStyle() == Typeface.BOLD) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    @Override
+    public void italic(int start, int end, boolean format) {
 
+    }
 
+    @Override
+    public boolean containItalic(int start, int end) {
+        StyleSpan[] spans = getEditableText().getSpans(start, end, StyleSpan.class);
+        for (StyleSpan span : spans) {
+            if (span.getStyle() == Typeface.ITALIC) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    @Override
+    public void underLine(int start, int end, boolean format) {
+
+    }
+
+    @Override
+    public boolean containUnderLine(int start, int end) {
+        UnderlineSpan[] spans = getEditableText().getSpans(start, end, UnderlineSpan.class);
+        if(spans != null && spans.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void strikeThrough(int start, int end, boolean format) {
+
+    }
+
+    @Override
+    public boolean containStrikeThrough(int start, int end) {
+
+    }
+
+    @Override
+    public void addCheckBox() {
+
+    }
 }
